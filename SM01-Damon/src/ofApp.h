6165c980933f936fb1309e18df13cc89d480a47b
Damon Chen <hnc247@nyu.edu>
2025-02-10 16:55:52 -0500

    SM01- Game Of Life

---
+++ SM01-Damon/src/ofApp.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		int cellSize;      // Size (in pixels) of each cell block when drawn.
+		int gridWidth;     // Number of cells along the x-axis.
+		int gridHeight;    // Number of cells along the y-axis.
+
+		ofImage lifeImg;   // The image holding Game of Life grid.
+		
+};
